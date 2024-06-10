e55529a5ecdecfe3755c64115860b77d81501d6c
Tom Dodd <joedodd35@gmail.com>
2024-06-10 05:44:29 +0100

    Add processor TE and GUI classes

--- src/main/java/nclegacy/Global.java
+++ src/main/java/nclegacy/Global.java
@@ -4,6 +4,6 @@ public class Global {
 	
 	public static final String MOD_ID = "nclegacy";
 	public static final String MOD_NAME = "NCO Legacy Lib";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String DEPENDENCIES = "required-after:nuclearcraft";
 }
